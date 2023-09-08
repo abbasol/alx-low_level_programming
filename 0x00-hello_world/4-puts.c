653c54e4eb66d0593b4aa3788ef90ec7fa16d28c
abbasol <abbas_fu@outlook.sa>
2023-09-07 17:39:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints a given phrase
- * Return: 0 if exited properly, non-zero otherwise 
+ * main - this is the main  function
+ * Return: always 0
  */
 
 int main(void)
