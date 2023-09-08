65a2b51999eeb9ecce47427b60673e0dd4a62d47
abbasol <abbas_fu@outlook.sa>
2023-09-07 18:22:58 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - prints exactly "and that piece of art is useful"
- *  - Dora korpar, 2015-10-19",
- *  followed bu a new line, to the standard error.
- *  Return: Always 0(Success)
+ * main - this prints exactly the input on it
+ *  Return: Alway 1.
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
