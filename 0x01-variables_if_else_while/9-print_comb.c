e0e75862e0743dc6a73e7f9e54904ef8cab0b47e
cdkali <abbas_fu@outlook.sa>
2023-09-10 03:05:56 -0400

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
         int number;
 
-	for (number = 0; number <= 9; number ++)
+	for (number = 0; number <=8;  number ++)
 	{
 	putchar(number + 0'0);
-	if (number < 9)
+	if (number < 8)
 	{
 	putchar(',');
 	putchar(' ');
