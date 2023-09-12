575d53170f20b2428b23237d43e97822cab44f90
cdkali <abbas_fu@outlook.sa>
2023-09-12 18:30:10 -0400

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 			 * put space if product is a single number
 			 * place the first digit if its two numbers
 			 */
-			if (prod <=9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48); /*get the first digit*/
