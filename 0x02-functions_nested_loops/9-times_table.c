c68c0036a0291bd0ee4f9687e899090071d236f7
cdkali <abbas_fu@outlook.sa>
2023-09-12 18:20:07 -0400

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (num = 0; num <= 9; num++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
