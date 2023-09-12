3d73314b7b4e45011e95bd6d8d55de5f83e5d098
cdkali <abbas_fu@outlook.sa>
2023-09-12 06:17:49 -0400

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function to print
- *                  the alphabet a - z
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void
  */
+
 void print_alphabet(void)
 {
 	int ch;
