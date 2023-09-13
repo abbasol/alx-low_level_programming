c5c0c85bc7502efd188fd2eaac7ad1d78fab4e9f
cdkali <abbas_fu@outlook.sa>
2023-09-12 21:47:33 -0400

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,8 @@
  * _islower - function to check if
  *            character is lowercase
  *
+ * @c: checks input of function
+ *
  * Return: return 1 if 'c' is lowercase
  * otherwise always 0 (Success)
  */
