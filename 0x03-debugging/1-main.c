8825661f787064a1693007883ab603b07364f2c1
cdkali <abbas_fu@outlook.sa>
2023-09-14 20:28:18 -0400

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,6 +7,7 @@
 
 int main(void)
 {
+
 	int i;
 	
 	printf("Infinite loop incoming :(\n");
@@ -15,8 +16,7 @@ int main(void)
 	/*while (i < 10)*/
 	/*{*/
 	/*     putchar(i);
-	 *     *}
-	/ *}*/
+	/*}*/      
 	
 	printf("Infinite loop avoided! \\o/\n");
 	
