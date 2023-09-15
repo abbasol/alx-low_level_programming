44d18c460fd0c74b3da70bd6c37ae8a4e5bcb830
cdkali <abbas_fu@outlook.sa>
2023-09-15 19:38:20 -0400

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * print_square - print a square
+ * print_square - prints a square
  * @size: print size
  * Return: Always 0.
  */
 
 void print_square(int size)
 {
+
 int i;
 int k;
 if (size > 0)
@@ -19,6 +20,14 @@ for (k = 0; k < size; k++)
 _putchar (35);
 }
 _putchar('\n');
+
+}
+
+}
+
+else
+{
+_putchar ('\n')
 }
 
 }
