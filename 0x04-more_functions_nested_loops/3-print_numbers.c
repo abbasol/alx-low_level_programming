7e4eff3ecc2b3a8152496eacc643016501376542
cdkali <abbas_fu@outlook.sa>
2023-09-15 16:16:55 -0400

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
 /**
- * print_numbers - print 0 - 9
- *             only using _putchar twice
- *
- * Return: Always 0 (Success)
+ * print_numbers - print numbers
+ * Return: Always 0.
  */
-
 void print_numbers(void)
 {
-	int num = 0;
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
-	_putchar('\n');
+int i;
+
+i = 0;
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
+}
+
+_putchar('\n');
 }
