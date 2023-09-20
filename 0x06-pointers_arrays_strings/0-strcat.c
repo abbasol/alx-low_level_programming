862e68c691ec71ce8a112d7b790f1a08a7c5b16d
cdkali <abbas_fu@outlook.sa>
2023-09-20 12:21:13 -0400

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _strcat - concatenates two string
  * @dest: input value
  * @src: input value
@@ -24,7 +23,6 @@ char *_strcat(char *dest, char *src);
 		i++;
 		j++;
 	}
-
-	dest [i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
