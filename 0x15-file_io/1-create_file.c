27c56544e914ecfaa73e625399830174408853ac
abbasol <abbas_fu@outlook.sa>
2023-10-30 17:19:38 -0400

    all tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file
  * @filename: filename.
- * @text_content: content content writed in the file.
+ * @text_content: content writed in the file.
  *
  * Return: 1 if it success. -1 if it fails.
  */
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, 0 CREAT | 0 WRONLY | 0 TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
